83fd20a0368ed38687bd01cd936ba86b2bbb87ea
dell <dell@DESKTOP-I89L11M>
2024-02-08 22:39:33 +0530

    push code notebook,long notebook

---
+++ src/main/java/pksp1/notebook.java
@@ -0,0 +1,10 @@
+package pksp1;
+
+public class notebook {
+
+	public static void main(String[] args) {
+		System.out.println("i am push the code origin");
+
+	}
+
+}
---
+++ src/test/java/pkgsp2/longnoteBook.java
@@ -0,0 +1,10 @@
+package pkgsp2;
+
+public class longnoteBook {
+
+	public static void main(String[] args) {
+		System.out.println(" i am push the code longnoteBook");
+
+	}
+
+}
